8916bcd6a3bc9686166c7ee2a486a1134f5486f7
Parminder Singh Gill <133721736+gillPrince7@users.noreply.github.com>
2023-12-08 23:06:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ if uploaded_image is not None:
 
 
 
-    model_final = keras.models.load_model('aug_model_1.h5')
+    model_final = keras.models.load_model('class_model.h5')
     st.write(model_final.summary())
     # Load and preprocess your image (img) here
 
